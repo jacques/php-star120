03c04ed805e359bc587f1acf47e8ba6c9107c944
Jacques Marneweck <jacques@powertrip.co.za>
2018-04-09 07:23:49 +0200

    Config for phpunit

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Star120 Debit Order Parsers and Exporters
+ *
+ * @author    Jacques Marneweck <jacques@siberia.co.za>
+ * @copyright 2018 Jacques Marneweck.  All rights reserved.
+ */
+
+require_once __DIR__.'/../vendor/autoload.php';
